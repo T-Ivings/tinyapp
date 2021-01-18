12b9bdb720ee6d6bbef597cb2aeae4b2a12eb6d4
Taylor <ivingstaylor@gmail.com>
2021-01-18 22:06:28 +0000

    first post request, added a random string generator to express_server, and updated other files

--- express_server.js
+++ express_server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  const alphanumeric = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let results = "";
+  for (let x = 6; x > 0; x--) {
+    results += alphanumeric.charAt(Math.round(Math.random() * 61) + 1)
+  }
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,17 +24,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
 });
 
 
